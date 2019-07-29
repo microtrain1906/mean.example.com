1465e01c7b4e7e372c7d13bb1489aa8f5b737f34
chad-svastisalee <swasgrafx@gmail.com>
2019-07-29 15:03:45 -0500

    Add location.hash detection for app navigation

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -25,71 +25,113 @@ var authApp = (function() {
   
       app.innerHTML=form;
     }
-
-    function registrationForm(){
-        var app = document.getElementById('app');
-    
-        var form =  `
-            <div class="card login-form">
-              <form id="registrationForm" class="card-body">
-                <h1 class="card-title text-center">Create an Account</h1>
-                <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
-    
-                <div class="form-group">
-                  <label for="first_name">First Name</label>
-                  <input type="text" id="first_name" name="first_name" class="form-control" required>
-                </div>
-    
-                <div class="form-group">
-                  <label for="last_name">Last Name</label>
-                  <input type="text" id="last_name" name="last_name" class="form-control" required>
-                </div>
-    
-                <div class="form-group">
-                  <label for="username">Username</label>
-                  <input type="text" id="username" name="username" class="form-control" required>
-                </div>
-    
-                <div class="form-group">
-                  <label for="email">Email</label>
-                  <input type="text" id="email" name="email" class="form-control" required>
-                </div>
-    
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" id="password" name="password" class="form-control" required>
-                </div>
-    
-                <div class="form-group">
-                  <label for="confirm_password">Confirm Password</label>
-                  <input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
-                </div>
-    
-                <div>
-                  <input type="submit" value="Sign In" class="btn btn-lg btn-primary btn-block">
-                </div>
-              </form>
-            </div>
-        `;
   
-          app.innerHTML=form;
+    function registrationForm(){
+      var app = document.getElementById('app');
+  
+      var form =  `
+          <div class="card login-form">
+            <form id="registrationForm" class="card-body">
+              <h1 class="card-title text-center">Create an Account</h1>
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+  
+              <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" id="first_name" name="first_name" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" id="last_name" name="last_name" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" class="form-control" required>
+              </div>
+  
+              <div class="form-group">
+                <label for="confirm_password">Confirm Password</label>
+                <input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
+              </div>
+  
+              <div>
+                <input type="submit" value="Sign In" class="btn btn-lg btn-primary btn-block">
+              </div>
+            </form>
+          </div>
+      `;
+  
+      app.innerHTML=form;
+    }
+  
+    function postRequest(formId, url){
+      let form = document.getElementById(formId);
+  
+      form.addEventListener('submit', function(e){
+  
+        e.preventDefault();
+  
+        let formData = new FormData(form);
+        let uri = `${window.location.origin}${url}`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', uri);
+  
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+  
+        let object = {};
+        formData.forEach(function(value, key){
+          object[key]=value;
+        });
+  
+        xhr.send(JSON.stringify(object));
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          if(data.success===true){
+            window.location.href = '/';
+          }else{
+            document.getElementById('formMsg').style.display='block';
+          }
         }
-    
-    
-      return {
-        load: function(){
-        //    loginForm();
-        //    postRequest('loginForm', '/api/auth/login');
-           registrationForm();
-           postRequest('registrationForm', '/api/auth/register');
-           validate.registrationForm();
+      });
+    }
+  
+    return {
+      load: function(){
+  
+        switch(window.location.hash){
+          case '#register':
+            registrationForm();
+            postRequest('registrationForm', '/api/auth/register');
+            validate.registrationForm();
+            break;
+  
+          default:
+            loginForm();
+            postRequest('loginForm', '/api/auth/login');
+            break;
         }
+  
       }
-          
+    }
+  
   })();
   
   var validate = (function() {
-
+  
     function confirmPasswordMatch() {
   
       let pw = document.getElementById('password');
@@ -108,13 +150,17 @@ var authApp = (function() {
         document.querySelector('#registrationForm input[type="submit"]').addEventListener(
           'click',
           function(){
+          validateEmail();
           confirmPasswordMatch();
         });
       }
     }
   
-
-    })();
-
-
+  })();
+  
+  
   authApp.load();
+  
+  window.addEventListener("hashchange", function(){
+    authApp.load();
+  });
